a2483595f62331e59088a03dc11cdb1bbeafa59e
baconYao <bacon735392@gmail.com>
2017-05-18 05:54:10 +0800

    Add mongodb connection

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const engine = require('ejs-mate');
 const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/rateme');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -13,6 +17,14 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'baconyaoSecretKey',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.conection})
+}));
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
